216f33f5b9b3f21eb86cdff55a1a359494853402
luca <luca@ubuntu.ubuntu-domain>
2014-03-12 14:40:19 +0100

    Aggiunto connessione

---
+++ Connessione.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="luca"
+__date__ ="$12-mar-2014 14.39.44$"
+
+if __name__ == "__main__":
+    print "Hello World"
